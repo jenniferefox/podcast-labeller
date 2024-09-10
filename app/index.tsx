e70046626a0e572ae6bdbc0c409a051c592eca5e
Jennifer Fox <jenniferelizfox@gmail.com>
2024-09-11 00:07:11 +0100

    Add react-native-track-player dependency
    Remove expo-router, and set-up our own index and App entrypoint so that we can register the TrackPlayer service.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,6 @@
-import { Text, View } from "react-native";
+import { registerRootComponent } from 'expo';
+import TrackPlayer from 'react-native-track-player';
+import App from './App';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+registerRootComponent(App);
+TrackPlayer.registerPlaybackService(() => require('./service'));
